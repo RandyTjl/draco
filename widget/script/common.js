75de2c28f6419c87396ce1e6528ce267b09b6e4c
RandyTjl <1101274814@qq.com>
2018-06-14 00:33:50 +0800

    update

--- widget/script/common.js
+++ widget/script/common.js
@@ -4,3 +4,14 @@ function getRandnum(n,m){
 		var c = m-n+1;
 		return Math.floor(Math.random()*c+n);
 }
+//返回到指定页面
+function returnWin(win_name){
+	//如果存在返回页面则返回到指定页面，否则返回到上一页
+	if(win_name){
+		api.closeToWin({
+				name: win_name
+		});
+	}else{
+		api.closeWin();
+	}
+}
