10b33eed5699171e788d632da39843d1f114da18
RandyTang(唐佳力) <randytang@fih-foxconn.com.cn>
2018-08-31 17:10:57 +0800

    update

--- widget/script/common.js
+++ widget/script/common.js
@@ -60,7 +60,7 @@ function ajaxForm(url,method,data,callback) {
         headers:headers,
         data:data,
     }, function(ret) {
-        api.alert({ title: '操作失败', msg:JSON.stringify(str) });
+        api.alert({ title: '操作失败', msg:JSON.stringify(ret) });
         if(ret.status){
             callback(ret);
         }else{
